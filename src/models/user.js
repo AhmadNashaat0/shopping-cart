a4ab3812efb37c7ae9e0d5a7a182d58acbd6d68a
ahmed nashaat <ahmednashaat721@yahoo.com>
2021-09-25 18:43:43 +0200

    user v1

--- src/models/user.js
+++ src/models/user.js
@@ -1,47 +1,104 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 const userSchema  = new mongoose.Schema({
     name:{
-        type: String,
-        required: true,
-        trim: true
+        first:{
+            type: String,
+            required: true,
+            trim: true
+        },last:{
+            type: String,
+            required: true,
+            trim: true
+        }
     },
     username:{
         type: String,
         required: true,
-        trim: true
+        unique: true,
+        trim: true,
     },
     email:{
         type: String,
         required: true,
         trim: true,
         unique:true,
-        lowercase: true
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
+        }
     },
     password:{
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        validate(value) {
+            if (validator.isStrongPassword(value)>=30) {
+                throw new Error('password not strong enough');
+            }
+        }
     },
     age:{
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        validate(value){
+            if(value<0){
+                throw new Error('age is invalid') ;
+            }
+        }
     },
-    token: {
-        type: String,
-        required: true
+    admin:{
+        type: Boolean,
     },
-    cart:{
-        products:[{
-            
-        }]
-    }
+    tokens: [{
+        token:{
+            type: String,
+            required: true
+        }
+    }]
 },{
    timestamps: true 
 });
 
+// virtuals
+userSchema.virtual('fullName').get(function(){
+    return this.name.first + ' ' + this.name.last;
+});
 
+// methods
+userSchema.methods.makeToken = function () {
+    const token = jwt.sign({ _id: this._id.toString(),admin:this.admin}, process.env.JWT_SECRET);
+    this.tokens = this.tokens.concat({ token });
+    return token;
+};
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+// static functions
+userSchema.statics.login = async function(email, password){
+    //const user = await User.findOne({ $or : [{username:email},{email:email.lowercase}]});
+    const user = await User.findOne({email:email.lowercase});
+    if (!user) {
+        throw new Error('email is invalid');
+    }
+
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+        throw new Error('worng password');
+    }
+    return user;
+};
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    next();
+});
+
+module.exports =  mongoose.model('Users', userSchema);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,36 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
 
-router.
 
-router.get('/',(req,res)=>{
+router.get('/:me',(req,res)=>{
+
     res.send('hi');
 });
 
+router.post('/register', async(req,res)=>{
+    try{
+        const user = new User(req.body);
+        const token = user.makeToken();
+        await user.save();
+        res.cookie('token',token);
+        res.status(201).send('added');
+    }catch(e){
+        res.status(400).send(e);
+    }
+});
 
-
-
+router.post('/login', async(req,res)=>{
+    try{
+        const user = await User.login(req.body.email, req.body.password);
+        const token = user.makeToken();
+        await user.save();
+        res.cookie('token',token);
+        res.send('logged');
+    }catch(e){
+        console.log(e);
+        res.status(400).send(e);
+    }
+});
 
 module.exports = router;

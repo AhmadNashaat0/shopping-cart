fefa7091c43427306b24fc8b44b17b255a4bf586
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-09-25 16:32:00 +0200

    adding products routes for products api

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,10 +1,62 @@
 const express = require('express');
-const router = new express.Router();
 const Product = require('../models/product');
+const router = new express.Router();
 
 
-router.get('/', (req, res) => {
-    res.send({ products: [] });
+router.get('/', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.send(products)
+    }
+
+    catch (e) {
+        console.log(e);
+        res.status(500).send(e)
+    }
+
+
+});
+
+router.get('/:productId', async (req, res) => {
+    const _id = req.params.productId;
+
+    try {
+        const product = await Product.findById(_id)
+        if (!product) {
+            return res.status(404).send({ error: "Can't Find this product" })
+        }
+
+        res.status(200).send({ product })
+    }
+    catch (e) {
+        res.status(500).send(e)
+    }
+
 })
+
+router.post("/", async (req, res) => {
+    const product = new Product(req.body)
+
+    try {
+        await product.save();
+        res.status(201).send(product)
+    }
+    catch (e) {
+        res.status(400).send(e.message)
+    }
+
+})
+
+/*
+TODO:
+        * ADD route for updating product
+        * Add route for Deleting product
+
+
+
+
+*/
+
+
 
 module.exports = router

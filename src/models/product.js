3319d4ab803ea953f12a926bfee4274a15040048
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-09-25 14:19:34 +0200

    adding product model

--- src/models/product.js
+++ src/models/product.js
@@ -1,1 +1,41 @@
 // name, id, desc, no., price
+
+const mongoose = require('mongoose');
+const validator = require('validator')
+
+
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        max: 20,
+        trim: true
+    },
+    description: {
+        type: String,
+        min: 10
+    },
+    stock: {
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 0)
+                throw new Error("Stock must be +ve number Only")
+        }
+
+    },
+    price: {
+        type: Number,
+        required: true,
+        validate(value) {
+            if (value < 0)
+                throw new Error("Price must be +ve number Only")
+        }
+
+    }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product

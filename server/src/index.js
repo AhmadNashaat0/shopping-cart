a7eccc4aa05efbafae197c65ebf7dcd902a5a58d
ahmed nashaat <ahmednashaat721@yahoo.com>
2021-09-27 13:30:32 +0200

    handle port

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ require('dotenv').config({path: require('path').resolve(__dirname,'../config/.en
 require('./db/db.js');
 require('./routers/index.js')(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => {
     console.log('server working on port',port);

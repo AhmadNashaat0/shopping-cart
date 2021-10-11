4041384a1b511655a0d991f0671bde9a5fc0bf0f
ahmadyasser01 <ahmadyasser2814@gmail.com>
2021-10-11 11:44:55 +0200

    add homescreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-// import Cart from "./screens/Cart";
+import Cart from "./screens/Cart";
 import Product from "./screens/Product";
 import "./App.css";
+import Home from "./screens/Home";
 
 
 const App = () => {
   return (
     <div>
-      <Product></Product>
+      {/* <Product></Product> */}
+      {/* <Cart></Cart> */}
+      <Home></Home>
     </div>
   );
 };
---
+++ client/src/screens/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ProductCard from "../components/ProductCard";
+
+const Home = () => {
+    return (
+        <div>
+            {/* Nav bar */}
+            <div className="home">
+                <h1>Products</h1>
+                <div className="home-products">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
